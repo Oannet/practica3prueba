4fd9fb57bcc7f337af85cf7d695659ee9dc425f4
Kevin-Antonio-Carvajal <Kevin-Antonio@ciencias.unam.mx>
2024-03-05 02:21:03 -0600

    hola2

--- analizador-sintactico-los-belicones/ejemplo/lexer.rkt
+++ analizador-sintactico-los-belicones/ejemplo/lexer.rkt
@@ -1,31 +1,97 @@
 #lang nanopass
+
 (require parser-tools/lex
-        (prefix-in : parser-tools/lex-sre))
+         (prefix-in : parser-tools/lex-sre))
 (provide (all-defined-out))
-
-; 2+3
-(define-tokens contenedores (NUM)) ; Tokens con atributo <NUM, 24>
-(define-empty-tokens vacios (ADD EOF MUL)) ; Tokens vacios <ADD>
-
-(define hola-lexer
-        (lexer
-            ;[trigger(expresion regular)   action(reportar error, reportar token, ignorar ...)]
-            [(:+ (char-range #\0 #\9))  (token-NUM (string->number lexeme))]
-            [(eof)      (token-EOF)]
-            [whitespace (hola-lexer input-port)]
-            [(:: #\+)   (token-ADD)]
-            [(:: #\*)   (token-MUL)]))
-
-
-
-#|  Un lexema es una secuencia de caracteres del codigo fuente que coincide con el
-    patron de un token
-    Ejemplo de clase :
-        2+3
-        Entrada : +3 <EOF> $
-        Pila : 2
-        Tokens : <NUM, 24> <ADD>
-        Lexema 2
-        Token  NUM
-        <NUM,24> <ADD> <NUM,3>
+#|
+       INTEGRANTES:
+Aguilera Moreno Adrian.
+Rojas Reyes Saul Adrian.
+Aquino Chapa Armando Abraham.
+Alejandro Gutierrez Medina Sebastián.
 |#
+
+;; Lexer y Tokens.
+
+(define-tokens contenedores (ID BOOLEAN NUM)) ; Token para identificadores, e.g. var1 => (ID var1).
+(define-empty-tokens vacios (
+                                 LP      RP     ; (    )
+                                 LC      RC     ; [    ]
+                                 LL      RL     ; {    }
+                                 MAIN
+                                 IF
+                                 ELSE
+                                 LENGTH
+                                 WHILE
+                                 RETURN
+                                 SUM     REST   ; +    -
+                                 DIV     MULT   ; /    *
+                                 MOD     1D     ; %    ==
+                                 DIF     MEN    ; !=   <
+                                 MAY     ME1D   ; >    <=
+                                 MA1D    AND    ; >=   &
+                                 OR      NOT    ; |    !
+                                 ASIG           ; =
+                                 MASI    MENI   ; +=   -=
+                                 INC     DEC    ; ++   --
+                                 TYP     QIN    ; :    ?
+                                 COM            ; ,
+                                 INT     BOOL
+                                 EOF
+                             ))  ; Tokens de tipos, e.g. LP => (
+
+(define jelly-lex
+  (lexer
+   ; [(Exp. regular) (token)]
+   [" "   (jelly-lex input-port)]          ; Espacios vacíos.
+   [(:: "," )             (token-COM)    ] ; ,
+   [(:: "main" )          (token-MAIN)   ] ; if
+   [(:: "if" )            (token-IF)     ] ; else
+   [(:: "else" )          (token-ELSE)   ] ; then
+   [(:: "?" )             (token-QIN)    ] ; ?
+   [(:: "while" )         (token-WHILE)  ] ; while
+   [(:: "return" )        (token-RETURN) ] ; return
+   [(:: "length" )        (token-LENGTH) ] ; length
+   ["bool"                (token-BOOLEAN)] ; bool
+   ["int"                 (token-INT) ]
+   ["num"                 (token-NUM)    ] ; num
+   [(:: "(" )             (token-LP)     ] ; (
+   [(:: ")" )             (token-RP)     ] ; )
+   [(:: "[" )             (token-LC)     ] ; [
+   [(:: "]" )             (token-RC)     ] ; ]
+   [(:: "{" )             (token-LL)     ] ; {
+   [(:: "}" )             (token-RL)     ] ; }
+   [(:: ":" )             (token-TYP)    ] ; :
+   [(:= 1 "=")            (token-ASIG)   ] ; =
+   [(:= 1 "+")            (token-SUM)    ] ; +
+   [(:: "/")              (token-DIV)    ] ; /
+   [(:: "*" )             (token-MULT)   ] ; *
+   [(:= 1 "-")            (token-REST)   ] ; -
+   [(:: #\! )             (token-NOT)    ] ; !
+   [(:: "!=" )            (token-DIF)    ] ; !=
+   [(:: "==" )            (token-1D)     ] ; ==   
+   [(:: "%" )             (token-MOD)    ] ; %
+   [(:: "<=" )            (token-ME1D)   ] ; <=
+   [(:: ">=" )            (token-MA1D)   ] ; >=
+   [(:: "<" )             (token-MEN)    ] ; <
+   [(:: ">" )             (token-MAY)    ] ; >
+   [(:: "&" )             (token-AND)    ] ; &
+   [(:: "|" )             (token-OR)     ] ; |
+   [(:: "+=" )            (token-MASI)   ] ; +=
+   [(:: "++" )            (token-INC)    ] ; ++
+   [(:: "-=" )            (token-MENI)   ] ; -=
+   [(:: "--" )            (token-DEC)    ] ; --
+   
+   ; Identificadores.
+   [(:+ numeric) (token-NUM lexeme)]
+   [(:or "True" "False") (token-BOOLEAN lexeme)]
+   [(:: (char-range #\a #\z) (:* (:or numeric alphabetic #\_))) (token-ID lexeme)]
+   ; Por último agregamos la expresion regular para los comentarios
+   [(:: "{-" (complement (:: any-string "-}" any-string)) "-}") (jelly-lex input-port)]
+   [(:: "//" (complement (:: any-string "\n" any-string)) "\n") (jelly-lex input-port)]
+   [(eof)      (token-EOF)]))
+
+;; Función que lexea una cadena a nuestra gramática (función de prueba):
+(define (lexear s) (let* ([input (open-input-string s)])
+    (print (jelly-lex input))))
+; Ejemplo de prueba (escribir en terminal): (lexear "main() {5+3 4+3}")
--- analizador-sintactico-los-belicones/ejemplo/parser.rkt
+++ analizador-sintactico-los-belicones/ejemplo/parser.rkt
@@ -1,57 +1,261 @@
 #lang nanopass
-(require "lexer.rkt"
-          parser-tools/yacc)
 
+;; Cambiar -- por //
+;; es bool no boolean
 
-; ; 2+3
+; GRAMATICA DE MI LENGUAJE
+; expr    -> expr + expr | ! expr | constante | id | ( expr )
+; id      -> [a..z]+
+; consante <- [0..9]+
 
-;     +
-;    / \
-;   2   +
-;      / \
-;     3   4
+; ARBOL DE SINTAXIS CONCRETA PARA "3 + var"
+;         expr
+;        / | \
+;    expr  +  expr
+;     |        |
+;   const      id
+;     |        |
+;     3        var
 
-; (+ 2 (+ 3 4))
+; ARBOL DE SINTAXIS ABSTRACTA PARA 3 + (2 - 4)
+;         +
+;        / \
+;       3   -
+;          / \
+;         2   4
 
+(require "lexer_tokens.rkt"
+         parser-tools/yacc)
+(provide (all-defined-out))
 
+#|
+       INTEGRANTES:
+Aguilera Moreno Adrian.
+Rojas Reyes Saul Adrian.
+Aquino Chapa Armando Abraham.
+Gutierrez Medina Sebastián Alejandro.
+|#
 
-;     expr
-;     / | \
-;  expr + expr
-;   |      |
-;  const  const
-;   |      |
-;   2      3
+;ESTRUCTURAS PARA LOS NODOS DEL ARBOL
+(define-struct id (i) #:transparent) 
+(define-struct num (n) #:transparent)
+(define-struct bool (b) #:transparent)
+(define-struct return (exp) #:transparent)
+(define-struct length (exp) #:transparent)
+(define-struct arg (id tipo) #:transparent)
+(define-struct un-expr (op e) #:transparent)
+(define-struct if-corto (g t e) #:transparent)                ; ( ... ) ? 
+(define-struct call (nombre arg) #:transparent)
+(define-struct my-while (exp then) #:transparent)
+(define-struct bin-expr (op o1 o2) #:transparent)
+(define-struct main (instrucciones) #:transparent)
+(define-struct my-if (exp then else) #:transparent)
+(define-struct arreglo (id tipo tamano) #:transparent)           ; arreglo
+(define-struct procedimiento (nombre argumentos tipo instrucciones) #:transparent)
 
-
-
-
-(define-struct num (v) #:transparent)
-(define-struct bin-op (op v1 v2) #:transparent)
-
-; (bin-op ADD (NUM 2) (NUM 3))
-
-(define hola-parser
+(define jelly-parser
     (parser
-        [start expr]
-        [end EOF]
-        [tokens contenedores vacios]
-        [error (display "hola")]
-        [grammar
-            [const
-                [(NUM) (num $1)]]
-            [expr
-                [(expr ADD expr) (bin-op '+ $1 $3)]
-                [(expr MUL expr) (bin-op '* $1 $3)]
-                [(const)        $1]]]))
+     [start programa]
+     [end EOF]
+     [tokens contenedores vacios] 
+     [error ; procedimiento que tiene que seguir el parser cuando encuentre un error
+      (lambda (tok-ok? tok-name tok-value)
+        (raise-syntax-error 'error "Tu token no es admisible: "
+                            (if tok-value tok-value tok-name)))]
+       
+      [precs (nonassoc LP)     ; (
+             (nonassoc RP)     ; )
+             (nonassoc LC)     ; [
+             (nonassoc RC)     ; ]
+             (nonassoc LL)     ; {
+             (nonassoc RL)     ; }
+             (nonassoc IF)     ; if
+             (nonassoc ELSE)   ; else
+             (nonassoc NOT)    ; !
+             (nonassoc MAIN)   ; main
+             (nonassoc COM)    ; ,
+             
+            ; FALTA TYP ":"
+             (right RETURN)    ; return
+             (right INC)       ; ++
+             (right DEC)       ; --
+             (right QIN)       ; ?
+             (right ASIG)      ; =   
+             (right MASI)      ; +=
+             (right MENI)      ; -=
+             (left MULT)       ; *
+             (left DIV)        ; /
+             (left MOD)        ; %
+             (left SUM)        ; +
+             (left REST)       ; -
+             (left MEN)        ; <
+             (left MAY)        ; >
+             (left ME1D)       ; <=
+             (left MA1D)       ; >; while ( ... ) { ... }
+             (left 1D)         ; ==
+             (left DIF)        ; !=
+             (left AND)        ; &
+             (left OR)         ; |
+      ]
+      
+      [grammar
+        [programa
+            [(procedimientos) $1]
+            [(main) $1]
+            [(main procedimientos) (list* $1 $2)]]
+        [main
+            [(MAIN LP RP instrucciones) (main $4)]]
+        ; Obs. Quite los métodos y funciones y los metí aquí (pocos shift/reduce adelante):
+        [procedimiento
+         [(id LP declaraciones RP TYP tipo instrucciones) (procedimiento $1 $3 $6 $7)]
+        ]
+        ; Recursión de procedimiento:
+        [procedimientos
+         [(procedimiento) $1]
+         [(procedimiento procedimientos) (list* $1 $2)]
+        ]
+        
+        (id
+            [(ID) (id $1)])                         
+        (return
+            [(RETURN expresion) (return $2)])
+       
+        
+        [sentencias ; Acciones:  S -> ...
+         [(estructura) $1]
+         [(estructura sentencias) (list* $1 $2)]
+        ]
 
-; expr    :: const | expr + expr | expr * expr
-; const   :: 0 | 1 | 2 | 3 ... | 9
+        ; Diferentes estructuras:
+        [estructura  ; estructuras
+            [(if)              $1]
+            [(call)            $1]                       
+            [(while)           $1]                            
+            ;[(argumento)       $1]    ; Genera shift/reduce por declaraciones (No es necesario).
+            [(if-corto)        $1]     ; TODO. shift/reduce por causa del if
+            [(return)          $1]
+            [(length)          $1]
+            [(asignacion)      $1]                       
+            [(declaraciones)   $1]
+            [(instrucciones)   $1]
+        ] 
+        
+        ; length
+         [length
+          [(LENGTH LP id RP) (length $3)]
+         ]
 
+         ; if ( ... ) then { ... } else { ... }
+         [if
+          [(IF expresion estructura ELSE estructura)   (my-if $2 $3 $5)]
+         ]
 
+         ; ( ... ) ? { ... } : { ... }
+         [if-corto
+          [(LP expresion RP QIN estructura TYP estructura) (if-corto $2 $5 $7)]
+         ]
 
-(define (lex-this lexer input) (lambda () (lexer input)))
+         ; while ( ... ) { ... }
+         [while
+          [(WHILE expresion instrucciones) (my-while $2 $3)]
+         ]
+        
+         [asignacion
+          [(argumento ASIG expresion)               (bin-expr '= $1 $3)]
+          ; Asignación para arreglos con declaración incluida:
+          [(argumento ASIG LL constantes RL)        (bin-expr '= $1 $4)]
+          [(id ASIG expresion)                      (bin-expr '= $1 $3)]
+          ; Asignación múltiple:
+          [(id ASIG asignacion)                     (bin-expr '= $1 $3)]
+          ; Asignación de arreglo declarado (múltiple):
+          [(id ASIG LL constantes RL)               (bin-expr '= $1 $4)]
+         ]     
 
-(define (parsea in)
-        (let ([in-s (open-input-string in)])
-        (hola-parser (lex-this hola-lexer in-s))))
+         ; Declaraciones de argumentos y valores en general:
+         [declaraciones
+          [(argumento COM declaraciones) (list* $1 $3) ]
+          [(argumento)                   $1            ]
+         ]
+
+         ; { ... }
+         [instrucciones
+          [(LL RL)            empty ]
+          [(LL expresion RL)  $2    ]         ; TODO. shift/reduce   (Para que acepte cosas como: 5+5)
+          [(LL sentencias RL) $2    ]
+         ]
+
+         ; Distintos datos declarados
+         [argumento
+          [(id TYP tipo LC NUM RC)     (arreglo $1 $3 $5)   ] ; TODO. Agregarle id a la representación.
+          ; $1  $2  $3
+          ; id  :  tipo [   ]
+          [(id TYP tipo LC RC)         (arreglo $1 $3 empty) ]
+          [(id TYP tipo)               (arg $1 $3)       ]
+         ]
+
+         ; Valor constante:
+         [constante
+          [(NUM)      (num $1)]
+          [(BOOLEAN)  (bool $1)]
+         ]
+        
+         ; Recursión con constante:
+         [constantes
+          [(constante COM constantes)         (list* $1 $3)]
+          [(constante)                        (list $1)]
+         ]
+
+         ; llamadas, invocaciones, etc:
+         [call
+          [(id LP expresiones RP) (call $1 $3)]
+         ]
+
+         ; Expresion: puede ser una operación unaria o binaria:
+         [expresion ; TODO. Esto puede tronar
+          [(LP expresion RP)           $2                   ]                             
+          [(constante)                 $1                   ]
+          [(id)                        $1                   ]
+          [(expresion OR   expresion)  (bin-expr '\| $1 $3) ]      
+          [(expresion AND  expresion)  (bin-expr '&  $1 $3) ]      
+          [(expresion 1D   expresion)  (bin-expr '== $1 $3) ]      
+          [(expresion DIF  expresion)  (bin-expr '!= $1 $3) ]      
+          [(expresion MEN  expresion)  (bin-expr '<  $1 $3) ]        
+          [(expresion MAY  expresion)  (bin-expr '>  $1 $3) ]        
+          [(expresion MA1D expresion)  (bin-expr '>= $1 $3) ]      
+          [(expresion ME1D expresion)  (bin-expr '<= $1 $3) ]      
+          [(expresion SUM  expresion)  (bin-expr '+  $1 $3) ]        
+          [(expresion REST expresion)  (bin-expr '-  $1 $3) ]
+          [(expresion INC)             (bin-expr '+ $1 1)   ] ; Quitamos azucar sintáctica
+          [(expresion DEC)             (bin-expr '- $1 1)   ] ; Quitamos azucar sintáctica
+          [(expresion MASI expresion)  (bin-expr '+ $1 $3)  ] ; Quitamos azucar sintáctica
+          [(expresion MENI expresion)  (bin-expr '- $1 $3)  ] ; Quitamos azucar sintáctica
+          [(expresion MULT expresion)  (bin-expr '*  $1 $3) ]        
+          [(expresion DIV  expresion)  (bin-expr '/  $1 $3) ]        
+          [(expresion MOD  expresion)  (bin-expr '%  $1 $3) ]        
+          [(REST expresion)            (un-expr  '-  $2)    ]                
+          [(NOT expresion)             (un-expr  '!  $2)    ]
+          [(call)                      $1                   ]
+         ]
+
+         ; Recursión con expresion:
+         [expresiones
+          [(expresion COM expresiones)  (list* $1 $3)]
+          [(expresion) $1]
+         ]
+
+         ; Sistema de tipos:
+         [tipo
+          [(INT)    'INT]
+          [(BOOL)   'BOOL]
+         ]
+        ]
+    )
+)
+
+; Pruebas
+(define (lex lexer input) (lambda () (lexer input)))
+;(jelly-parser (lex jelly-lex (open-input-string "main() { if 5+3 > 4 { return 4+3} else {2} }")))
+;(jelly-parser (lex jelly-lex (open-input-string "main() { if 5+3 > 4 {return 4+3} else { return 2+3} a = b = c =  4+5 }")))
+;(jelly-parser (lex jelly-lex (open-input-string "main() { a = true}")))
+(jelly-parser (lex jelly-lex (open-input-string "main() {a: int[] = {1,2,3,4,5} a:int = 3}")))
+;(jelly-parser (lex jelly-lex (open-input-string "main() {length(a)}")))
